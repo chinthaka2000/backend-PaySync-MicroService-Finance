7b5da591b26d737c0dd28d76439f217389dbfe61
Sahran-mhd <Sahranmhd312@gmail.com>
2025-07-21 19:26:59 +0530

    create schema for loans

---
+++ backend/models/Grantor.js
@@ -0,0 +1,43 @@
+// models/Grantor.js
+const mongoose = require('mongoose');
+
+const grantorSchema = new mongoose.Schema({
+  grantorId: {
+    type: String,
+    required: true,
+    unique: true, // e.g., GR0001
+  },
+
+  personalInfo: {
+    fullName: String,
+    contactNumber: String,
+    email: String,
+    dateOfBirth: Date,
+    address: String, 
+  },
+
+  identityVerification: {
+    idType: { type: String, default: 'NIC' },
+    idNumber: String,
+    documentUrl: String
+  },
+
+  employmentDetails: {
+    employer: String,
+    jobRole: String,
+    monthlyIncome: Number,
+    employmentLetterUrl: String
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Grantor', grantorSchema);
